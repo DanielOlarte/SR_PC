84332c44513bc78388099f9ed636b3a5eaa22c1f
jsrc1115 <jsrc1987@gmail.com>
2013-03-06 02:37:30 -0500

    Fixing a commit error, didnt staged files: -SceneManager -SR_PC.sln

---
+++ Assets/Scripts/BattleArena/SceneManager.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using System.Collections;
+
+public class SceneManager : MonoBehaviour {
+	
+	private float levelLenght = 40.0f;
+	
+	public float getLevelLenght(){
+		return levelLenght;
+	}
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
